5a16484ceca4f56c21eb97b417ec40a99d56515d
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2023-06-02 09:44:41 -0300

    Adding sampling to runPrograms.py

--- tool/runPrograms.py
+++ tool/runPrograms.py
@@ -1,12 +1,27 @@
-from os import listdir
+from os import listdir, remove
+from random import sample, randrange, seed
+from sys import argv, stderr, maxsize
 import subprocess as sub
-import os as os
 
 if __name__ == '__main__':
-    sub.run('mkdir programs', shell=True)
-    for input in listdir('output'):
+    if len(argv) < 2:
+        print(f'Usage: {argv[0]} <num_inputs> [seed]', file=stderr)
+        exit(1)
+
+    if len(argv) == 3:
+        seed(int(argv[2]))
+    else:
+        rand_seed = randrange(maxsize)
+        seed(rand_seed)
+        print(rand_seed)
+
+    sub.run('mkdir -p programs', shell=True)
+    sample_size = int(argv[1])
+    inputs = listdir('./output')
+    for input in sample(inputs, sample_size):
         sub.run('clang output/' + input + ' -std=c99 -o programs/' + input[:-2], shell=True) 
-    sub.run('mkdir prog_out', shell=True)
+
+    sub.run('mkdir -p prog_out', shell=True)
     for input in listdir('programs'):
         sub.run('./programs/'+input + ' 0 > prog_out/'+input+'0.txt', shell=True)
         sub.run('./programs/'+input + ' 1 > prog_out/'+input+'1.txt', shell=True)
@@ -14,9 +29,10 @@ if __name__ == '__main__':
         sub.run('./programs/'+input + ' 3 > prog_out/'+input+'3.txt', shell=True)
         sub.run('./programs/'+input + ' 4 > prog_out/'+input+'4.txt', shell=True)
         sub.run('./programs/'+input + ' 5 > prog_out/'+input+'5.txt', shell=True)
+
     for input in listdir('prog_out'):
         input_file = open('prog_out/' + input, 'r', encoding='unicode_escape')
         line = input_file.readline().strip()
         if line == 'Usage:':
-            os.remove('prog_out/' + input)
+            remove('prog_out/' + input)
         input_file.close()
