dc8c225783131fdc44f054281dd12740677bd5fc
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2023-06-07 21:03:30 -0300

    Updating script to generate CSV file

--- tool/runInstrumentation.py
+++ tool/runInstrumentation.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from os import listdir
 from subprocess import run
 
@@ -5,14 +6,31 @@ if __name__ == '__main__':
     run('rm -rf output', shell=True)
     inputs = listdir('../test/jotai_benchmarks')
     print(len(inputs))
+
+    results = []
     errors = []
     for input in inputs:
         print(f'Running: {input}')
-        proc = run(f'./run.sh ../test/jotai_benchmarks/{input} {input}', shell=True, capture_output=True, text=True)
+        proc = run(
+            f'./run.sh ../test/jotai_benchmarks/{input} {input}', shell=True, capture_output=True, text=True)
+
+        lines = list(map(lambda x: x.strip(), proc.stdout.split('\n')))
+        not_instrumented = len(lines) > 1 and lines[1] == 'Unable to instrument the input'
+        results.append({
+            'name': input,
+            'func_size': int(lines[0]),
+            'instrumented': not not_instrumented
+        })
+
         if proc.returncode != 0:
             errors.append((input, proc.stderr))
 
-    print('\n\n======================ERRORS======================\n\n')
-    for (input, err) in errors:
-        print(f'======================{input}======================')
-        print(err)
+    if len(errors) > 0:
+        print('\n\n======================ERRORS======================\n\n')
+        for (input, err) in errors:
+            print(f'======================{input}======================')
+            print(err)
+
+    columns = ['name', 'func_size', 'instrumented']
+    df = pd.DataFrame(data=results, columns=columns)
+    df.to_csv('experimentRQ1.csv', mode='a', header=False, index=False)
