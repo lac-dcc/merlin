088798f75b111e5e182d3e41d71a51ac7a3dc7c3
1Lucas.Victor <1victor.lucassilva@gmail.com>
2023-07-10 13:47:07 -0300

    Removing Interpolation

--- interpolation/include/PolynomialInterpolation.hpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <vector>
-
-
-//Calculates an interpolation for single-variable functions.
-std::string NewtonInterpolation(std::vector<double>& x, std::vector<std::vector<double>>& table, int n);
-
-
-double calculateDeterminant(const std::vector<std::vector<double>>& matrix, int);
