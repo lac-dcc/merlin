98b9b639522f9cca6f3538559a7a984478b2d8e7
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2023-08-04 14:58:00 -0300

    Refactoring and documenting bash scripts

--- instrumentation/scripts/run.sh
+++ instrumentation/scripts/run.sh
@@ -1,41 +1,53 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 2 ]
-then
-    echo 'Syntax: run.sh input_file output_file'
+# This script runs Merlin's instrumentation for a given C/C++ input. The program
+# is formatted using clang-format before running the actual instrumentation. The
+# instrumented program is saved in the output/ directory.
+# 
+# Notice that you must set the variable LLVM_BUILD_DIR to the directory where
+# LLVM is built in your computer.
+#
+# Usage: ./run.sh <input_file> <output_file>
+#
+# Arguments: 
+#   input_file: Path to the input file.
+#   output_file: Name of the output file.
+
+if [ $# -lt 2 ]; then
+    echo 'Usage: run.sh <input_file> <output_file>'
     exit 1
-else
-    LLVM_BUILD_DIR="$HOME/llvm-project/build"
-    CLANG_FORMAT="$LLVM_BUILD_DIR/bin/clang-format"
-    LIB="./build/src/libMerlin.so"
-    INPUT=$1
-    OUTPUT=$2
+fi
 
-    FILE=$(basename $INPUT)
-    EXT="${FILE##*.}"
-    case $EXT in
-        "cc" | "cpp") CC="$LLVM_BUILD_DIR/bin/clang++" ;;
-        "c") CC="$LLVM_BUILD_DIR/bin/clang" ;;
-    esac
+LLVM_BUILD_DIR="$HOME/llvm-project/build"
+CLANG_FORMAT="$LLVM_BUILD_DIR/bin/clang-format"
+LIB="./build/src/libMerlin.so"
+INPUT=$1
+OUTPUT=$2
 
-    mkdir -p output
+FILE=$(basename $INPUT)
+EXT="${FILE##*.}"
+case $EXT in
+    "cc" | "cpp") CC="$LLVM_BUILD_DIR/bin/clang++" ;;
+    "c") CC="$LLVM_BUILD_DIR/bin/clang" ;;
+esac
 
-    TEMP=$FILE
-    $CLANG_FORMAT $INPUT --style="{BasedOnStyle: llvm, InsertBraces: true}" > $TEMP
-    
-    $CC -std=c99 -fsyntax-only -Xclang -load -Xclang $LIB -Xclang -plugin -Xclang merlin \
-    -Xclang -plugin-arg-merlin  -Xclang -output-file -Xclang -plugin-arg-merlin -Xclang $OUTPUT \
-    $TEMP
+mkdir -p output
 
-    RET_VAL=$?
+TEMP=$FILE
+$CLANG_FORMAT $INPUT --style="{BasedOnStyle: llvm, InsertBraces: true}" > $TEMP
 
-    rm $TEMP
+$CC -std=c99 -fsyntax-only -Xclang -load -Xclang $LIB -Xclang -plugin -Xclang merlin \
+-Xclang -plugin-arg-merlin  -Xclang -output-file -Xclang -plugin-arg-merlin -Xclang $OUTPUT \
+$TEMP
 
-    if [ $RET_VAL -ne 0 ]; then
-        exit 1
-    fi
+RET_VAL=$?
 
-    if test -f "output/$OUTPUT"; then
-        $CLANG_FORMAT "output/$OUTPUT" -i --style="{BasedOnStyle: llvm}" 
-    fi
+rm $TEMP
+
+if [ $RET_VAL -ne 0 ]; then
+    exit 1
+fi
+
+if test -f "output/$OUTPUT"; then
+    $CLANG_FORMAT "output/$OUTPUT" -i --style="{BasedOnStyle: llvm}" 
 fi
--- instrumentation/scripts/setup.sh
+++ instrumentation/scripts/setup.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-# =========================================================================== #
+
+# This script builds Merlin's instrumentation. All necessary files are saved in
+# the build/ directory.
+#
+# Notice that you must set the variable LLVM_BUILD_DIR to the directory where
+# LLVM is built in your computer.
+#
+# Usage: ./script <input_file> <output_file>
+
 LLVM_BUILD_DIR="$HOME/llvm-project/build"
 
 mkdir -p build
@@ -17,4 +25,3 @@ cmake -S . -B $BUILD_DIR -G "Ninja"              \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .
 
 ninja -j16 -C $BUILD_DIR
-# =========================================================================== #
