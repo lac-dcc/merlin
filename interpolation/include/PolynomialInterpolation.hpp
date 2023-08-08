46e34a79dcad58d2fb513539025d73879734d9a6
1Lucas.Victor <1victor.lucassilva@gmail.com>
2023-08-08 20:46:42 -0300

    Refactoring Interpolation

--- interpolation/include/PolynomialInterpolation.hpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <vector>
-
-
-//Calculates an interpolation for single-variable functions.
-std::string NewtonInterpolation();
-
-//Calculates an interpolation for 2-variable functions.
-std::string solve_2var();
--- interpolation/src/main.cpp
+++ interpolation/src/main.cpp
@@ -1,41 +1,72 @@
+#include "interpolator.hpp"
+#include <vector>
 #include <iostream>
-#include "PolynomialInterpolation.hpp"
+#include <sstream>
+
 
 using namespace std;
 
-
-
-// C*P
-// pointsA[]
-// pointsB[]
-
-// points = vector<vector<int>>(counters);
-// for( i : num_points) {
-//     for (j : counters) {
-//         points[j].push_back(i);
-//     }
-// }
-
 int main() {
-  int N, numVar;
-  string result;
+  int numInterp, numPoints;
+  cin >> numInterp;
+  cin >> numPoints;
+  cin.ignore();
+  vector<vector<string>> interpVars(numInterp);
+  vector<vector<double>> xValues (numInterp); // X variables
+  vector<vector<double>> yValues (numInterp); // Y variable
+  vector<vector<double>> fValues (numInterp); // Result - F(x)/F(x,y) = z
 
-  std::cin >> numVar;
+  for (int i = 0; i < numInterp; i++) {
+    string varNames;
+    getline(cin, varNames);
+    varNames = varNames.substr(varNames.find(':') + 1, varNames.length());
 
-  switch(numVar) {
-    case 0:
-      result = "Complexity not influenced by a variable. O(1)";
-      break;
-    case 1:
-      result = "F(x) = " + NewtonInterpolation();
-      break;
-    case 2:
-      result = solve_2var();
-      break;
-    default:
-      result = "Bad input or case not implemented.";
+    if (varNames.size() <= 0)
+      continue;
+
+    stringstream varStream(varNames);
+    string name;
+    while (varStream >> name) {
+      interpVars[i].push_back(name);
+    }
   }
 
-  cout << result << '\n';
+  for (int i = 0; i < numPoints; i++) {
+    for (int j = 0; j < numInterp; j++) {
+        double xi, yi, zi;
+        switch (interpVars[j].size())
+        {
+        case 1:
+            cin >> xi >> zi;
+            xValues[j].push_back(xi);
+            fValues[j].push_back(zi);
+            break;
+        case 2:
+            cin >> xi >> yi >> zi;
+            xValues[j].push_back(xi);
+            yValues[j].push_back(yi);
+            fValues[j].push_back(zi);
+            break;
+        default:
+            break;
+        }
+    }
+  }
 
+  for(int i = 0; i < numInterp; i++) {
+    int numVars = interpVars[i].size();
+    if(numVars == 1) {
+      Interpolator interp (xValues[i], fValues[i]);
+      cout << "x: " << interpVars[i][0] << endl;
+      cout << interp.interpolate() << endl << endl;
+    } 
+    else if(numVars == 2) {
+      Interpolator interp (xValues[i], yValues[i], fValues[i]);
+      cout << "x: " << interpVars[i][0] << endl;
+      cout << "y: " << interpVars[i][1] << endl;
+      cout << interp.interpolate() << endl << endl;
+    }
+  }
+
+  return 0;
 }
