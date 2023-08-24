263a9430d3fe22bb1c24d8b4b5244c061ef49adf
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2023-08-24 13:15:51 -0300

    Renewing sample of size 50

---
+++ experiments/sample/extr_kittykittenschoosescore.c_has_char.c
@@ -0,0 +1,119 @@
+// ========================================================================= //
+
+// includes
+#include "float.h"
+#include "limits.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "time.h"
+
+#define JOTAI_NUM_RANDS_ 25
+
+const unsigned rand_primes[JOTAI_NUM_RANDS_] = {
+    179,   103,   479,    647,    229,    37,     271,   557,   263,
+    607,   18743, 50359,  21929,  48757,  98179,  12907, 52937, 64579,
+    49957, 52567, 507163, 149939, 412157, 680861, 757751};
+
+int next_i() {
+  int counter = 0;
+  return rand_primes[(++counter) % JOTAI_NUM_RANDS_];
+}
+
+float next_f() {
+  int counter = 0;
+  return rand_primes[(++counter) % JOTAI_NUM_RANDS_] / 757751.0F;
+}
+
+// Usage menu
+void usage() {
+  printf("%s", "Usage:\n\
+    prog [ARGS]\n\
+\nARGS:\n\
+       0            big-arr-10x\n\
+\n\
+");
+}
+
+// ------------------------------------------------------------------------- //
+
+#define NULL ((void *)0)
+typedef unsigned long size_t; // Customize by platform.
+typedef long intptr_t;
+typedef unsigned long uintptr_t;
+typedef long scalar_t__; // Either arithmetic or pointer type.
+/* By default, we understand bool (as a convenience). */
+typedef int bool;
+#define false 0
+#define true 1
+
+/* Forward declarations */
+
+/* Type definitions */
+typedef scalar_t__ text_t;
+typedef size_t len_t;
+
+/* Variables and functions */
+
+__attribute__((used)) static inline bool has_char(text_t *text, len_t sz,
+                                                  text_t ch) {
+  unsigned counterhas_char0 = 0;
+
+  len_t tempsz = sz;
+
+  for (len_t i = 0; i < sz; i++) {
+    counterhas_char0++;
+    if (text[i] == ch) {
+
+      printf("1\n");
+      printf("at line 60 :");
+      printf(" sz\n");
+      printf("%ld %d\n", tempsz, counterhas_char0);
+      return true;
+    }
+  }
+
+  printf("1\n");
+  printf("at line 60 :");
+  printf(" sz\n");
+  printf("%ld %d\n", tempsz, counterhas_char0);
+  return false;
+}
+
+// ------------------------------------------------------------------------- //
+
+// ------------------------------------------------------------------------- //
+
+int main(int argc, char *argv[]) {
+
+  if (argc != 2) {
+    usage();
+    return 1;
+  }
+
+  int opt = atoi(argv[1]);
+  switch (opt) {
+
+  // big-arr-10x
+  case 0: {
+    unsigned long sz = 10;
+    long ch = 10;
+    int _len_text0 = 100;
+    long *text = (long *)malloc(_len_text0 * sizeof(long));
+    for (int _i0 = 0; _i0 < _len_text0; _i0++) {
+      text[_i0] = ((-2 * (next_i() % 2)) + 1) * next_i();
+    }
+    int benchRet = has_char(text, sz, ch);
+    printf("%d\n", benchRet);
+    free(text);
+
+    break;
+  }
+
+  default:
+    usage();
+    break;
+  }
+
+  return 0;
+}
