e142d3919d1f19ac82e5c85a1d999730736fdc71
1Lucas.Victor <1victor.lucassilva@gmail.com>
2023-10-02 10:14:06 -0300

    Adding Interpolation Results
    Co-authored-by: Rafael Sumitani <rafasumi@users.noreply.github.com>

--- interpolation/runInterpolation.py
+++ interpolation/runInterpolation.py
@@ -3,13 +3,14 @@ from sys import argv, stderr
 from os import listdir
 
 if __name__ == '__main__':
-    if len(argv) < 2:
+    if len(argv) < 3:
         print(f'Usage: {argv[0]} <input_dir>', file=stderr)
         exit(1)
     
     input_dir = argv[1]
-    run(f'mkdir -p ./output/{input_dir}', shell=True)
-    # for input in listdir(input_dir):
-    #     print(f'Running Interpolation for {input}')
-    #     run(f'./bin/interpolator < {input_dir}/{input} > output/{input_dir}/{input}', shell=True)
+    out_name = argv[2]
+    run(f'mkdir -p ./output/{out_name}', shell=True)
+    for input in listdir(input_dir):
+        print(f'Running Interpolation for {input}')
+        run(f'./bin/interpolator < {input_dir}/{input} > output/{out_name}/{input}', shell=True)
 
