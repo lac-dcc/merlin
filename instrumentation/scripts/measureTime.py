605788804e269a8b4d13e34009841aa0832d1a61
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2024-01-08 19:25:07 -0300

    Update script for measuring execution time

--- instrumentation/scripts/measureTime.py
+++ instrumentation/scripts/measureTime.py
@@ -14,34 +14,45 @@ Usage:
 from os import listdir, remove
 from subprocess import run
 
+import pandas as pd
+
 from find_func_name import find_func_name
 
+TEMP_FILE = 'total_time.tmp'
+NUM_RUNS = 3
 
-def run_instrumentation_and_measure_time(inputs):
-    temp_file = 'total_time.tmp'
+
+def run_program(program, results):
     ignore_non_newton = 'false'
     measure_time = 'true'
+    func_target = find_func_name(program)
+
+    command = f'./scripts/run.sh \
+{benchmark_dir}/{program} {program} {func_target} {ignore_non_newton} {measure_time}'
+    run(command, shell=True, capture_output=True, text=True)
+
+    # Clear the contents of the temp file
+    open(TEMP_FILE, 'w').close()
+
+    command = f'/usr/bin/time -f %e -a -o {TEMP_FILE} ./scripts/run.sh \
+{benchmark_dir}/{program} {program} {func_target} {ignore_non_newton} {measure_time}'
 
     times = []
-    for input in inputs:
-        func_target = find_func_name(input)
-
-        command = f'/usr/bin/time -f %e -a -o {temp_file} ./scripts/run.sh \
-{benchmark_dir}/{input} {input} {func_target} {ignore_non_newton} {measure_time}'
+    for _ in range(NUM_RUNS):
         proc = run(command, shell=True, capture_output=True, text=True)
 
         lines = list(map(lambda x: x.strip(), proc.stdout.split('\n')))
+        size = int(lines[0])
         times.append(int(lines[1]))
 
-    instrumentation_time_micro = sum(times)
+    avg_instrumentation_time_micro = sum(times) / NUM_RUNS
 
-    with open(temp_file, 'r') as file:
+    with open(TEMP_FILE, 'r') as file:
         times = file.readlines()
-        total_time = sum(map(lambda x: float(x), times))
+        avg_total_time = sum(map(lambda x: float(x), times)) / NUM_RUNS
 
-    remove(temp_file)
-
-    return instrumentation_time_micro, total_time
+    results.append({'program': program, 'inst_time': avg_instrumentation_time_micro,
+                    'total_time': avg_total_time, 'ast_size': size})
 
 
 if __name__ == '__main__':
@@ -51,18 +62,20 @@ if __name__ == '__main__':
     instrumentation_times = []
     total_times = []
 
-    run_instrumentation_and_measure_time(inputs)
-    for _ in range(3):
-        instrumentation_time_micro, total_time = run_instrumentation_and_measure_time(
-            inputs)
-        instrumentation_times.append(instrumentation_time_micro)
-        total_times.append(total_time)
+    results = []
 
-    avg_instrumentation_time_milli = (
-        sum(instrumentation_times) / len(instrumentation_times)) / 1000
-    avg_total_time = sum(total_times) / len(total_times)
+    for input in inputs:
+        run_program(input, results)
 
-    with open('exp_RQ5.txt', mode='w') as file:
+    remove(TEMP_FILE)
+
+    df = pd.DataFrame(data=results, columns=results[0].keys())
+    df.to_csv('./exp_RQ5.csv', mode='w', header=True, index=False)
+
+    with open('./exp_RQ5.txt', mode='w') as file:
+        avg_instrumentation_time_milli = df['inst_time'].sum() / 1000
+        avg_total_time = df['total_time'].sum()
+
         file.write(
             f'Average Instrumentation Time: {avg_instrumentation_time_milli:.3f}ms\n')
         file.write(f'Average Total Processing Time: {avg_total_time:.3f}s\n')
