0383f806d10ee0858fe46089e9fd1521a69c27a6
Rafael Sumitani <rafaelsumitani@dcc.ufmg.br>
2024-01-30 10:18:56 -0300

    Improving merlin.sh script

--- merlin.sh
+++ merlin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ $# -lt 3 ]; then
-  echo 'Usage: run.sh <program_file> <target_function> <input_dir>'
+  echo 'Usage: merlin.sh <program_file> <target_function> <input_dir>'
   exit 1
 fi
 
@@ -12,11 +12,21 @@ EXT="${PROGRAM##*.}"
 OUTPUT="temp.$EXT"
 RUN_SCRIPT="./scripts/run.sh"
 
+# ----------------------- INSTRUMENTATION -----------------------
+
 cd instrumentation
+
+printf "Compiling instrumentation... "
 SETUP_OUT=$(./scripts/setup.sh 2>&1)
-INST_OUT=$($RUN_SCRIPT "../$PROGRAM" $OUTPUT $TARGET false false)
+printf "done\n"
+
+printf "Instrumenting target program... "
+INST_OUT=$($RUN_SCRIPT "../$PROGRAM" $OUTPUT $TARGET)
+printf "done\n"
 
 cd output
+
+# ----------- COMPILING AND RUNNING THE TARGET PROGRAM -----------
 
 case $EXT in
 "cc" | "cpp") CC="clang++" ;;
@@ -30,15 +40,22 @@ for i in ../../"$INPUT_DIR"*; do
   TEMP_OUTPUT+="$(./temp <"$i")\n"
 done
 
+# ------------------------ INTERPOLATION ------------------------
+
 rm temp*
 INPUT_FILE=temp_input.txt
 echo -e "$TEMP_OUTPUT" >../../interpolation/$INPUT_FILE
 
 cd ../../interpolation
+printf "Compiling interpolation... "
 MAKE_OUT=$(make 2>&1)
+printf "done\n"
+
 
 python3 produceInput.py $INPUT_FILE $INPUT_FILE
 rm $INPUT_FILE
 
+printf "Final output:\n\n"
 ./bin/interpolator <input/$INPUT_FILE
+
 rm input/$INPUT_FILE
